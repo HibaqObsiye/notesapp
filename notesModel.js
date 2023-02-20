e633225303c13f84ff9657742b6d72729cbe7b0b
hibaq obsiye <obsiyehibaq@gmail.com>
2023-02-20 11:30:51 +0000

    failing test for notes model

---
+++ notesModel.js
@@ -0,0 +1,4 @@
+class NotesModel{
+
+}
+module.exports = NotesModel;
--- notesModel.test.js
+++ notesModel.test.js
@@ -1,1 +1,8 @@
 const NotesModel = require('./notesModel')
+
+it('adds a note', ()=>{
+    const model = new NotesModel();
+    model.addNote('buy milk')
+
+    expect(model.getNotes()).toBe('buy milk')
+})
