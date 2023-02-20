1041d0ec0f0bc8f8c20a724bd21b65b240e007a2
hibaq obsiye <obsiyehibaq@gmail.com>
2023-02-20 11:39:34 +0000

    test passing for multiple notes

--- notesModel.test.js
+++ notesModel.test.js
@@ -1,8 +1,17 @@
 const NotesModel = require('./notesModel')
 
-it('adds a note', ()=>{
+it('adds a note and returns it', ()=>{
     const model = new NotesModel();
     model.addNote('buy milk')
 
     expect(model.getNotes()).toEqual(['buy milk'])
+})
+
+it('returns multiple notes', () =>{
+    const model = new NotesModel();
+    model.addNote('buy milk')
+    model.addNote('buy bread')
+    model.addNote('buy tissues')
+
+    expect(model.getNotes()).toEqual(['buy milk', 'buy bread', 'buy tissues'])
 })
