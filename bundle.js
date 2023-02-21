a10b0eb1c541b81481dc8338d87d9f9407a82299
hibaq obsiye <obsiyehibaq@gmail.com>
2023-02-21 11:17:36 +0000

    required notes in index.js file

--- bundle.js
+++ bundle.js
@@ -1,4 +1,35 @@
 (() => {
+  var __getOwnPropNames = Object.getOwnPropertyNames;
+  var __commonJS = (cb, mod) => function __require() {
+    return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+  };
+
+  // notesModel.js
+  var require_notesModel = __commonJS({
+    "notesModel.js"(exports, module) {
+      var NotesModel = class {
+        constructor() {
+          this.notes = [];
+        }
+        addNote(note) {
+          this.notes.push(note);
+        }
+        getNotes() {
+          return this.notes;
+        }
+        resetNotes() {
+          return this.notes.length = 0;
+        }
+      };
+      module.exports = NotesModel;
+    }
+  });
+
   // index.js
+  var notesModel = require_notesModel();
+  model = new notesModel();
+  model.addNote("buy milk");
+  model.addNote("buy food");
+  console.log(model.getNotes());
   console.log("The note app is running");
 })();
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+const notesModel = require('./notesModel')
+
+model = new notesModel()
+model.addNote('buy milk')
+model.addNote('buy food')
+console.log(model.getNotes())
 console.log("The note app is running")
