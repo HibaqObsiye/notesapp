e4e610c047dcf420bab317a3184e52f357773a6e
hibaq obsiye <obsiyehibaq@gmail.com>
2023-02-26 21:17:47 +0000

    added event listener to button so message displays with each click

--- NotesView.js
+++ NotesView.js
@@ -1,9 +1,17 @@
 class NotesView{
     constructor(notes){
         this.notepad = notes
+        this.button = document.querySelector('#show-message-button')
+        this.button.addEventListener('click', () => {
+           this.displayNotes();
+        })
     }
 
     displayNotes(){
+        let inputValue = document.getElementById("message-input").value
+        console.log(inputValue)
+        this.notepad.addNote(inputValue)
+
         this.notepad.getNotes().forEach(element =>{
            let newDiv = document.createElement('div')
            newDiv.textContent = element
--- bundle.js
+++ bundle.js
@@ -31,8 +31,15 @@
       var NotesView2 = class {
         constructor(notes) {
           this.notepad = notes;
+          this.button = document.querySelector("#show-message-button");
+          this.button.addEventListener("click", () => {
+            this.displayNotes();
+          });
         }
         displayNotes() {
+          let inputValue = document.getElementById("message-input").value;
+          console.log(inputValue);
+          this.notepad.addNote(inputValue);
           this.notepad.getNotes().forEach((element) => {
             let newDiv = document.createElement("div");
             newDiv.textContent = element;
